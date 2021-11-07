eb5c2d7fa256d91f49f407dd7e0922f962a47dc9
Montse Viyuela <montsemsanz@gmail.com>
2021-11-07 17:33:44 +0100

    Proyecto de partida inicial para Entrega2-UT4-PracticaBucles

---
+++ PracticaBucles.java
@@ -0,0 +1,100 @@
+import java.util.Random;
+/**
+ *    
+ */
+public class PracticaBucles {
+    private final char ESPACIO = ' ';
+    private final char CARACTER = '\u0040';
+    private Random generador;
+
+    /**
+     * Constructor  
+     */
+    public PracticaBucles()    {
+        generador = new Random();
+    }
+
+    /**
+     *  El método  genera  números aleatorios  entre -1000 y 5000 (inclusive)
+     *  y calcula y escribe:
+     *      - la media de todos los números
+     *      - la suma de los impares
+     *      - el máximo de los pares
+     *        
+     *  El proceso termina cuando sale un 0 o bien se han escrito n nºs
+     *  
+     *  Los números se escriben  en filas de 5 columnas 
+     *  Cada nº aleatorio se muestra en un espacio de 12 posiciones en la pantalla  
+     *  y al lado de cada nº se muestra otro nº con las mismas cifras y 
+     *  en el mismo orden pero sin ceros (en 5 posiciones)
+     *  
+     *   Utiliza solo bucles while
+     */
+    public void generarNumeros(int n)   {
+       //TODO
+
+    }
+
+    /**
+     *  Devuelve true si numero es impar, false en otro caso
+     *  Hazlo sin utilizar if
+     */
+    public boolean esImpar(int numero)   {
+        //TODO
+        
+        
+        return  false;
+    }
+
+    /**
+     *  Dado un nº genera y devuelve otro nuevo 
+     *  con las mismas cifras y en el mismo orden pero sin 
+     *  los 0
+     *  Ej - si numero = 2040 devuelve  24, si numero = 1009 devuelve  19
+     *  si numero = 3000 devuelve 3
+     *   
+     *   
+     */
+    public int obtenerNumeroSinCeros(int numero)   {
+        //TODO
+        
+        
+        return 0;
+    }
+
+    /**
+     *  Borrar la pantalla
+     *
+     */
+    public void borrarPantalla()    {
+        System.out.println('\u000C');
+    }
+
+    /**
+     *  
+     *  Dibuja la letra N (ver figura en el enunciado)
+     *  Con bucles for
+     *  
+     *  Hay que usar el método escribirCaracter()
+     *  
+     *  
+     *   
+     */
+    public void escribirLetraN(int altura)    {
+       //TODO
+
+    }
+
+    /**
+     *  escribe n veces el caracter  indicado en la misma línea
+     *  con bucles for
+     */
+    private void escribirCaracter(char caracter, int n)    {
+       //TODO
+       
+       
+       
+       
+    }
+
+}
---
+++ TestPracticaBucles.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+/**
+ * Incluye todo el código dentro del main()
+ * No declares ningún nuevo método en esta clase ni atributos
+ *  
+ */
+public class TestPracticaBucles
+{
+
+    /**
+     *  Punto de entrada a la aplicación
+     *  - define y crea el teclado  
+     *  - define e instancia un objeto PracticaBucles
+     *  - pide al usuario la cantidad máxima de aleatorios a generar
+     *    (valida que sea  un nº positivo)
+     *  - muestra las estadísticas de números aleatorios
+     *  - haz una pausa y borra la pantalla    
+     *  
+     *  - pide al usuario un valor para la altura de la letra N -
+     *          Valida que este valor es correcto, es decir,
+     *          está en el intervalo 3 <= altura <= 10
+     *  - muestra la letra N
+     *  
+     */
+    public static void main(String[] args)    {
+        //TODO
+        
+        
+        
+        
+    }
+}
+
